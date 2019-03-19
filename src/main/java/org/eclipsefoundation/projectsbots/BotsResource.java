be81ae5f3ea219b40a922815eb8bc5e25982eeb1
Mikaël Barbero <mikael.barbero@gmail.com>
2019-03-19 15:55:17 +0100

    Added openapi documentation

--- src/main/java/org/eclipsefoundation/projectsbots/BotsResource.java
+++ src/main/java/org/eclipsefoundation/projectsbots/BotsResource.java
@@ -24,7 +24,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/")
+@Path("/bots")
 @Produces(MediaType.APPLICATION_JSON)
 public class BotsResource {
 
