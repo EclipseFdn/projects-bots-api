fb71b3be3e9d4b6afef6cf5b90b3e7b05c9b473a
Mikaël Barbero <mikael.barbero@gmail.com>
2019-03-19 13:08:25 +0100

    Initial commit

---
+++ src/test/java/org/eclipsefoundation/projectsbots/BotsTest.java
@@ -0,0 +1,31 @@
+package org.eclipsefoundation.projectsbots;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class BotsTest {
+
+    @Test
+    public void testGetByUsername() {
+        given()
+          .when().get("bots?username=che-bot")
+          .then()
+             .statusCode(200)
+             .body(is("[{\"kind\":\"github\",\"id\":1,\"email\":\"che-bot@eclipse.org\",\"projectId\":\"ecd.che\",\"username\":\"che-bot\"}]"));
+    }
+    
+    @Test
+    public void testGetById() {
+    	
+    	given()
+      .when().get("bots/2")
+      .then()
+         .statusCode(200)
+         .body(is("{\"kind\":\"github\",\"id\":2,\"email\":\"sprotty-bot@eclipse.org\",\"projectId\":\"ecd.sprotty\",\"username\":\"eclipse-sprotty-bot\"}"));
+    }
+
+}
---
+++ src/test/java/org/eclipsefoundation/projectsbots/NativeBotsIT.java
@@ -0,0 +1,9 @@
+package org.eclipsefoundation.projectsbots;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeBotsIT extends BotsTest {
+
+    // Execute the same tests but in native mode.
+}
