31cff91500588f45eac3f27e6864dd00d5517db8
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-06-28 11:29:51 +0200

    Use checked-in bots.db.json file as input for gen_bot_db.sh
    * Show an error if diff does not find a difference
    * Commit changes in bots.db.json automatically

--- regen_db.sh
+++ regen_db.sh
@@ -18,12 +18,40 @@ OLD_JSON="bots.db.old.json"
 NEW_JSON="bots.db.json"
 
 pushd "${SCRIPT_FOLDER}"
-kubectl exec -n foundation-internal-webdev-apps "$(kubectl get -n foundation-internal-webdev-apps pod -l "app=projects-bots-api,environment=production" -o json | jq -r ".items[0]|.metadata.name")" -- cat /deployments/bots.db.json | jq -S 'sort_by(.id)' > "${OLD_JSON}"
+
+echo "* Pulling latest version of projects-bots-api..."
+git pull
+
+#TODO: don't update if the bot has been added before
+cp "${NEW_JSON}" "${OLD_JSON}"
 ./gen_bot_db.sh "${OLD_JSON}" > bots.db.new.jsonnet
 jsonnet src/main/jsonnet/extensions.jsonnet | jq -S 'sort_by(.id)' > "${NEW_JSON}"
 rm -f bots.db.new.jsonnet
 
 diff "${OLD_JSON}" "${NEW_JSON}" || true
 
+#Show error if files are equal
+if diff "${OLD_JSON}" "${NEW_JSON}"; then
+  printf "\n\n"
+  echo "ERROR: diff found no difference. Please double check if something is missing!"
+  exit 1
+fi
+
 printf "\nRun 'diff %s %s' before deploying first!!\n" "${OLD_JSON}" "${NEW_JSON}"
+
+printf "\n\n"
+
+read -rsp $'Once you are done with comparing the diff, press any key to continue...\n' -n1
+
+echo "* Committing changes to projects-bots-api repo..."
+git add bots.db.json
+git commit -m "Update bots.db.json"
+git push
+
+echo "* Commit should trigger a build of https://foundation.eclipse.org/ci/webdev/job/projects-bots-api/job/master..."
+echo
+echo "* TODO: Wait for the build to finish..."
+printf "* TODO: Double check that bot account has been added to API (https://api.eclipse.org/bots)...\n"
+read -rsp $'Once you are done, press any key to continue...\n' -n1
+
 popd
